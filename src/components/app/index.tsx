7807f5b7870e2f09eda334920995164a53cf6eb4
Hilder Gill <hildergill@gmail.com>
2023-02-24 07:47:29 +0800

    Code formatting

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
 
-const App:FC=()=>{
-    return(<>
-    <h1>Hello, World!</h1>
-    <p>This app is still under development...</p>
-    </>)
-}
+const App: FC = () => {
+	return (
+		<>
+			<h1>Hello, World!</h1>
+			<p>This app is still under development...</p>
+		</>
+	);
+};
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import  { StrictMode } from 'react'
-import  { createRoot } from 'react-dom/client'
-import App from './components/app';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/app";
 
-const rootElement=document.getElementById("root")!!,
-root=createRoot(rootElement);
+const rootElement = document.getElementById("root")!!,
+	root = createRoot(rootElement);
 
 root.render(
-  <StrictMode>
-<App/>
-  </StrictMode>
+	<StrictMode>
+		<App />
+	</StrictMode>
 );
