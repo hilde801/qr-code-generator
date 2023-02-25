3cf003d4348f665fb5628b14b069faf0ca9ea09b
Hilder Gill <hildergill@gmail.com>
2023-02-25 22:28:24 +0800

    Added IconWarningTriangle from Tabler Icons

---
+++ src/components/erroroutput/index.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+import IconWarningTriangle from "../../icons/iconwarningtriangle";
+
+const ErrorOutput: FC<Error> = (props: Error) => {
+	return (
+		<>
+			<div>
+				<IconWarningTriangle />
+				<strong>Error</strong>
+			</div>
+			
+			<p>{props.message}</p>
+		</>
+	);
+};
+
+export default ErrorOutput;
