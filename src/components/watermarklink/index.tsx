f9ac84c1d70a856893fd24e338685b0fc2b0064a
Hilder Gill <hildergill@gmail.com>
2023-02-27 12:40:55 +0800

    Added styles for WatermarkLink

--- src/components/watermarklink/index.tsx
+++ src/components/watermarklink/index.tsx
@@ -1,9 +1,10 @@
 import { FC } from "react";
 import WatermarkLinkProps from "./props";
+import Style from "./style.module.css";
 
 const WatermarkLink: FC<WatermarkLinkProps> = (props: WatermarkLinkProps) => {
 	return (
-		<a href={props.href} download={props.download}>
+		<a href={props.href} download={props.download} className={Style.watermarkLink}>
 			<props.icon />
 			<p>{props.text}</p>
 			<props.icon />
