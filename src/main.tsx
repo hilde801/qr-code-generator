c6704671e49d6bfebafba776a4844cc601027e92
hilde801 <165758371+hilde801@users.noreply.github.com>
2024-07-21 05:18:08 +0800

    Some code cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const rootElement = document.getElementById("root")!,
+	root: ReactDOM.Root = ReactDOM.createRoot(rootElement);
+
+root.render(
 	<React.StrictMode>
 		<h1>Hello, World!</h1>
 		<p>This application is still under development...</p>
