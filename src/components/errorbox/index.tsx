2c2ce3fe7782ed0d6567cd0b763f56903ca47790
hilde801 <165758371+hilde801@users.noreply.github.com>
2024-07-21 08:26:04 +0800

    Added license stubs

--- src/components/errorbox/index.tsx
+++ src/components/errorbox/index.tsx
@@ -1,3 +1,8 @@
+// =============================================================================
+// Copyright 2024 Hilde801 (https://github.com/hilde801)
+// This file is a part of qr-code-generator
+// =============================================================================
+
 import React from "react";
 
 import Props from "./props";
--- src/components/errorbox/props.ts
+++ src/components/errorbox/props.ts
@@ -1,3 +1,8 @@
+// =============================================================================
+// Copyright 2024 Hilde801 (https://github.com/hilde801)
+// This file is a part of qr-code-generator
+// =============================================================================
+
 export interface Props {
 	error: Error;
 }
