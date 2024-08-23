f3ade6c9c734131cf0b06bcf5c300d8cbec56613
hilde801 <165758371+hilde801@users.noreply.github.com>
2024-08-23 21:31:39 +0800

    Icon URI fixes

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -9,7 +9,7 @@ import Styles from "./styles.module.css";
 const Header: React.FC = () => {
 	return (
 		<header className={Styles.header}>
-			<img className={Styles.icon} src="/icons/icon.png" />
+			<img className={Styles.icon} src="icons/icon.png" />
 			<h1 className={Styles.titleText}>QR Code Generator</h1>
 			<p>A simple web application to generate QR codes</p>
 		</header>
