25bd31afca1fb8f560c1dcaa4d9fc13b606cef9c
hilde801 <165758371+hilde801@users.noreply.github.com>
2024-08-25 07:37:48 +0800

    Added license stubs

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,3 +1,8 @@
+// =============================================================================
+// Copyright 2024 Hilde801 (https://github.com/hilde801)
+// This file is a part of qr-code-generator
+// =============================================================================
+
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 
